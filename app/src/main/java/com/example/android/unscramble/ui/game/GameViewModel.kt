4d07380d8f117305d4785bb3f867e859f5aa52b8
Fariz <farizbakur@gmail.com>
2023-09-21 10:55:06 +0700

    moving variable score, currentwordcound, currentscarbledword from gamefragment to gameviewmodel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,5 +4,8 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
+    private var score = 0
+    private var currentWordCount = 0
+    private var currentScrambledWord = "test"
 
 }
