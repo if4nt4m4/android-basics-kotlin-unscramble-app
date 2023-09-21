84656f8de471a8b88cf9c9130a841277993b3844
if4nt4m4 <kresnabayu164@gmail.com>
2023-09-22 06:20:48 +0700

    menambahkan variable score dan merapikan kode

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,6 +4,18 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+    private var _score = 0
+    val score: Int
+        get() = _score
+
+    private var currentWordCount = 0
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
     init {
         Log.d("GameFragment", "GameViewModel created!")
         getNextWord()
@@ -14,14 +26,6 @@ class GameViewModel : ViewModel() {
         Log.d("GameFragment", "GameViewModel destroyed!")
     }
 
-    private var score = 0
-    private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
-    val currentScrambledWord: String
-        get() = _currentScrambledWord
-
-    private var wordsList: MutableList<String> = mutableListOf()
-    private lateinit var currentWord: String
     init {
         getNextWord()
     }
