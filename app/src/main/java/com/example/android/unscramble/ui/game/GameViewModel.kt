330757b26a2cd7f08f74e319aba23a9355891494
Fariz <farizbakur@gmail.com>
2023-09-21 11:00:46 +0700

    change the currentScrambledWord to _currentScrambledWord

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -6,6 +6,8 @@ class GameViewModel : ViewModel() {
 
     private var score = 0
     private var currentWordCount = 0
-    private var currentScrambledWord = "test"
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
 
 }
