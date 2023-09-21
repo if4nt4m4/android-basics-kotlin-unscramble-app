04fd58e758b2f2d38b214670c4764efe1951aa85
if4nt4m4 <kresnabayu164@gmail.com>
2023-09-21 14:34:33 +0700

    add onCleared

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -8,6 +8,11 @@ class GameViewModel : ViewModel() {
         Log.d("GameFragment", "GameViewModel created!")
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
     private var score = 0
     private var currentWordCount = 0
     private var _currentScrambledWord = "test"
