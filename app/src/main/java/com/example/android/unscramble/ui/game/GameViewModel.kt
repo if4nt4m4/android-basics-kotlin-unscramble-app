dca830f6efa0e796dadfd42b8c4a35abdacfa04d
if4nt4m4 <kresnabayu164@gmail.com>
2023-09-22 07:23:05 +0700

    menambahkan increaseScore

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -47,6 +47,10 @@ class GameViewModel : ViewModel() {
         }
     }
 
+    private fun increaseScore() {
+        _score += SCORE_INCREASE
+    }
+
     fun nextWord(): Boolean{
         return if (currentWordCount < MAX_NO_OF_WORDS) {
             getNextWord()
