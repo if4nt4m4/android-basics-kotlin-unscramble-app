4eee80092603b39a8fc60059de84098a983a2433
if4nt4m4 <kresnabayu164@gmail.com>
2023-09-22 08:49:07 +0700

    menambahkan if pada onsubmitword

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -74,10 +74,12 @@ class GameFragment : Fragment() {
     private fun onSubmitWord() {
         val playerWord = binding.textInputEditText.text.toString()
 
-        if (viewModel.nextWord()) {
-            updateNextWordOnScreen()
-        } else {
-            showFinalScoreDialog()
+        if(viewModel.isUserWordCorrect(playerWord)) {
+            if (viewModel.nextWord()) {
+                updateNextWordOnScreen()
+            } else {
+                showFinalScoreDialog()
+            }
         }
     }
 
