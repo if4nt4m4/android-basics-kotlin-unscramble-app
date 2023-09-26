5e237d001244728b5f362a8696dad66bcc9d05ed
if4nt4m4 <kresnabayu164@gmail.com>
2023-09-26 14:45:34 +0700

    (Anugrah Gilang Ramadhan) Add binding expression to the current word

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -69,10 +69,7 @@ class GameFragment : Fragment() {
         // Update the UI
         updateNextWordOnScreen()
         // Observe the currentScrambledWord LiveData.
-        viewModel.currentScrambledWord.observe(viewLifecycleOwner,
-            {newWord ->
-                binding.textViewUnscrambledWord.text=newWord
-            })
+
         viewModel.score.observe(viewLifecycleOwner,
             { newScore ->
                 binding.score.text = getString(R.string.score, newScore)
